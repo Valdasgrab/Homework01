ac384ab1b4185a8b9994d36249c18a5e0d06070a
vgrabauskas <valdasgrab@gmail.com>
2023-03-29 19:36:30 +0300

    Adjusted functionality to appear on function

--- app/src/main/java/lt/vgrabauskas/homework01/MainActivity.kt
+++ app/src/main/java/lt/vgrabauskas/homework01/MainActivity.kt
@@ -1,7 +1,6 @@
 package lt.vgrabauskas.homework01
 
 import android.annotation.SuppressLint
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -18,45 +17,44 @@ class MainActivity : AppCompatActivity() {
 
         val button: Button = findViewById(R.id.okButton)
         button.setOnClickListener{
-            var nameEditText: TextView = findViewById(R.id.nameEditText)
-            var nameLength: Int = nameEditText.text.length
-            Log.i(TAG, "onCreate: $nameLength")
-            var numberOfLetters: TextView = findViewById(R.id.numberOfLettersTextView)
-            numberOfLetters.text = "The name contains $nameLength symbols"
-
-            val fuelPrice : TextView = findViewById(R.id.fuelPriceEditText)
-            val fuelFloat: Float = fuelPrice.text.toString().toFloat()
-            val fuelPriceChange: TextView = findViewById(R.id.fuelPriceTextView)
-            fuelPriceChange.text = "Fuel price is $fuelFloat Eur"
-
-            val sumtext: TextView = findViewById(R.id.sumTextView)
-            val sum: Float = nameLength + fuelFloat
-            sumtext.text = "Sum of symbols and fuel price is $sum"
-
-            val isSumPositive = sum > 0
-            val sumPositive: TextView = findViewById(R.id.isSumPositiveTextView)
-            sumPositive.text = "Is sum positive? $isSumPositive"
-
-            val firstLetter = nameEditText.text[0]
-            val firstLetterView: TextView = findViewById(R.id.firstLetterTextView)
-            firstLetterView.text = "First letter of the name is '$firstLetter' "
-
-            val subtraction: Float = nameLength - fuelFloat
-            val subtractionView: TextView = findViewById(R.id.subtractTextView)
-            subtractionView.text = "Result of subtraction $subtraction"
-
-            val isSumStillPositive = subtraction > 0
-            val sumStillPositiveView: TextView = findViewById(R.id.isStillPositiveTextView)
-            sumStillPositiveView.text = "Is sum still positive? $isSumStillPositive"
-
-            val charCode = firstLetter.code
-            val letterId: TextView = findViewById(R.id.letterIdPositiveTextView)
-            letterId.text = "The id of '$firstLetter' letter is $charCode "
+            nameAndFuel()
         }
-
-
-
     }
+    @SuppressLint("SetTextI18n")
+    fun nameAndFuel() {
+        val nameEditText: TextView = findViewById(R.id.nameEditText)
+        val nameLength: Int = nameEditText.text.length
+        Log.i(TAG, "onCreate: $nameLength")
+        val numberOfLetters: TextView = findViewById(R.id.numberOfLettersTextView)
+        numberOfLetters.text = "The name contains $nameLength symbols"
 
+        val fuelPrice : TextView = findViewById(R.id.fuelPriceEditText)
+        val fuelFloat: Float = fuelPrice.text.toString().toFloat()
+        val fuelPriceChange: TextView = findViewById(R.id.fuelPriceTextView)
+        fuelPriceChange.text = "Fuel price is $fuelFloat Eur"
 
+        val sumtext: TextView = findViewById(R.id.sumTextView)
+        val sum: Float = nameLength + fuelFloat
+        sumtext.text = "Sum of symbols and fuel price is $sum"
+
+        val isSumPositive = sum > 0
+        val sumPositive: TextView = findViewById(R.id.isSumPositiveTextView)
+        sumPositive.text = "Is sum positive? $isSumPositive"
+
+        val firstLetter = nameEditText.text[0]
+        val firstLetterView: TextView = findViewById(R.id.firstLetterTextView)
+        firstLetterView.text = "First letter of the name is '$firstLetter' "
+
+        val subtraction: Float = nameLength - fuelFloat
+        val subtractionView: TextView = findViewById(R.id.subtractTextView)
+        subtractionView.text = "Result of subtraction $subtraction"
+
+        val isSumStillPositive = subtraction > 0
+        val sumStillPositiveView: TextView = findViewById(R.id.isStillPositiveTextView)
+        sumStillPositiveView.text = "Is sum still positive? $isSumStillPositive"
+
+        val charCode = firstLetter.code
+        val letterId: TextView = findViewById(R.id.letterIdPositiveTextView)
+        letterId.text = "The id of '$firstLetter' letter is $charCode "
+    }
 }
