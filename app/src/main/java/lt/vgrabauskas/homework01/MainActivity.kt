82b4f9637bbbcbf5cb5f942d4620598b1c4df04e
v008s <valdasgrab@gmail.com>
2023-03-29 15:39:03 +0300

    Add number of letters

--- app/src/main/java/lt/vgrabauskas/homework01/MainActivity.kt
+++ app/src/main/java/lt/vgrabauskas/homework01/MainActivity.kt
@@ -1,12 +1,31 @@
 package lt.vgrabauskas.homework01
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    val TAG = "check_this"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        var nameLength: Int
+
+        val button: Button = findViewById(R.id.okButton)
+        button.setOnClickListener{
+            var nameEditText: TextView = findViewById(R.id.nameEditText)
+            var nameLength: Int = nameEditText.text.length
+            Log.i(TAG, "onCreate: $nameLength")
+            var numberOfLetters: TextView = findViewById(R.id.numberOfLettersTextView)
+            numberOfLetters.setText("The name contains $nameLength symbols")
+        }
+
+
+
     }
+
+
 }
